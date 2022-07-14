c6e6c8d71d5f3f2166360932b924499f9bac3458
Il Harper <hi@ilharper.com>
2022-07-15 06:07:10 +0800

    chore: update zsh config

--- zsh-ilharp.plugin.zsh
+++ zsh-ilharp.plugin.zsh
@@ -1,12 +1,35 @@
+
+# Set default editor
 export EDITOR='vim'
 
+# Set language
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+# Get root dir of zsh-ilharp
+ZSH_ILHARP_ROOT=$(dirname "$0")
+
+# Export ohmyzsh root dir
+export ZSH=${ZSH_ILHARP_ROOT}/bundles/oh-my-zsh
+
+# Load the oh-my-zsh's library
+# antigen use oh-my-zsh
+
+# Load bundles
+builtin source ${ZSH_ILHARP_ROOT}/bundles/oh-my-zsh/oh-my-zsh.sh
+builtin source ${ZSH_ILHARP_ROOT}/bundles/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+builtin source ${ZSH_ILHARP_ROOT}/bundles/zsh-completions/zsh-completions.plugin.zsh
+builtin source ${ZSH_ILHARP_ROOT}/bundles/powerlevel10k/powerlevel10k.zsh-theme
+
+# Load configs
+builtin source ${ZSH_ILHARP_ROOT}/config-zstyle.zsh
+builtin source ${ZSH_ILHARP_ROOT}/config-p10k.zsh
+
+# Common alias
 alias c='clear'
 alias e='exit'
 
-# Git
+# Git alias
 alias gst='git status'
 alias ga='git add'
 alias gaa='git add --all'
@@ -18,7 +41,6 @@ alias gc='git commit -v'
 alias gca='git commit -v --amend'
 alias gcmsg='git commit -m'
 
-# alias gd='git diff'
 # git diff with untracked
 gd() {
   (
