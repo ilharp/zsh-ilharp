2e3f84579901bb2615639f1397e58c70730b2163
Il Harper <hi@ilharper.com>
2023-08-12 15:23:35 +0800

    feat: add `dotnet` completion

--- zsh-ilharp.plugin.zsh
+++ zsh-ilharp.plugin.zsh
@@ -28,6 +28,24 @@ builtin source ${ZSH_ILHARP_ROOT}/config-p10k.zsh
 [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
 alias k='kubectl'
 
+# dotnet
+_dotnet_zsh_complete()
+{
+  local completions=("$(dotnet complete "$words")")
+
+  # If the completion list is empty, just continue with filename selection
+  if [ -z "$completions" ]
+  then
+    _arguments '*::arguments: _normal'
+    return
+  fi
+
+  # This is not a variable assignment, don't remove spaces!
+  _values = "${(ps:\n:)completions}"
+}
+
+compdef _dotnet_zsh_complete dotnet
+
 # rustup
 export RUSTUP_DIST_SERVER="https://mirrors.ustc.edu.cn/rust-static"
 export RUSTUP_UPDATE_ROOT="https://mirrors.ustc.edu.cn/rust-static/rustup"
