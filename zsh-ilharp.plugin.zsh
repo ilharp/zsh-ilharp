89b009c352373c6b7af54e14aa5fef4227dafdd0
Il Harper <hi@ilharper.com>
2023-12-27 01:08:03 +0800

    feat: update less config

--- zsh-ilharp.plugin.zsh
+++ zsh-ilharp.plugin.zsh
@@ -3,7 +3,7 @@
 export EDITOR='vim'
 
 # Enable less mouse support
-export LESS='-R --mouse --wheel-lines 3'
+export LESS='-FRSX --mouse --wheel-lines 3'
 
 # Set language
 export LC_ALL=en_US.UTF-8
