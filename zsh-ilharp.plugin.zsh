24a024c74433222f0f66cd9e37a48f4c6f49b3c5
Il Harper <hi@ilharper.com>
2025-03-28 01:11:52 +0800

    feat: add homebrew mirrors

--- zsh-ilharp.plugin.zsh
+++ zsh-ilharp.plugin.zsh
@@ -64,6 +64,13 @@ compdef _dotnet_zsh_complete dotnet
 export RUSTUP_DIST_SERVER="https://mirrors.tuna.tsinghua.edu.cn/rustup"
 export RUSTUP_UPDATE_ROOT="https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup"
 
+# Homebrew
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+export HOMEBREW_PIP_INDEX_URL="https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple"
+
 # Common alias
 alias c='clear'
 alias e='exit'
